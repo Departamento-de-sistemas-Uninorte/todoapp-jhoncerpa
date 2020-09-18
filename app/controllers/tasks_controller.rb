6fa4c62815cbffc8af4c9f6862593e2d4390c032
John Cerpa <jjcerpa@uninorte.edu.co>
2020-09-18 10:31:27 -0500

    Edit and show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,16 +6,40 @@ class TasksController < ApplicationController
 		# GET with blank form
     def new
       @task = Task.new    
-    end
+		end
+
+		def show
+			@task = Task.find(params[:id])
+		end
+
+		def edit
+			@task = Task.find(params[:id])
+		end
 
     # POST to save task in database
     def create
-			@task = Task.new(task_params)
+			@task = Task.new(task_params())
 			if @task.save
 				redirect_to tasks_path
 			else
 				render :new
 			end
+		end
+
+		def update
+			@task = Task.find(params[:id])
+
+			if @task.update(task_params)
+				redirect_to tasks_path
+			else
+				render :edit
+			end
+		end
+
+		def destroy
+			@task = Task.find(params[:id])
+			@task.destroy
+			redirect_to tasks_path
 		end
 		
 		private
